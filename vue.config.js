0a838f5205e83b8da1bbbd6e2ac457470d0404f1
PALMIS-SERVER <baniaga.eugene@gmail.com>
2022-07-14 20:46:20 +0800

    updates

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
+const fs = require('fs');
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? "/metronic8/vue/demo1/" : "/",
+    configureWebpack: {
+        devServer: {
+            allowedHosts: [
+                'dev-palmis.army.mil.ph'
+            ],		
+            host: 'dev-palmis.army.mil.ph',
+            compress:true,	
+            port: 8080,
+            https: {
+                key:fs.readFileSync('./keys/server.key'),
+                cert:fs.readFileSync('./keys/server.crt')
+            }
+            // for secured connection
+            //port: '443', https: true
+        }
+    },
     chainWebpack: config => {
         config
             .plugin('html')
