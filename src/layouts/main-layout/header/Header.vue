4c5e4055e981c5249b6b1d09861fb1be342162fd
Henrilics <henrydeguzman.java73@gmail.com>
2022-08-22 20:22:48 +0800

    up

--- src/layouts/main-layout/header/Header.vue
+++ src/layouts/main-layout/header/Header.vue
@@ -49,7 +49,7 @@
       <!--begin::Mobile logo-->
       <div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
         <router-link to="/" class="d-lg-none">
-          <img alt="Logo" src="media/logos/default-small.svg" class="h-30px" />
+          <img alt="Logo" src="/modules/pais-template/icons/logo64x64.png" class="h-30px" />
         </router-link>
       </div>
       <!--end::Mobile logo-->
