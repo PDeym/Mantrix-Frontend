ee82db4351ace3c3a2b96a19f661f5c7afa5ab47
Henrilics <henrydeguzman.java73@gmail.com>
2022-09-22 10:54:43 +0800

    up

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,7 @@ import { config } from "vuex-module-decorators";
 
 import AuthModule from "@/store/modules/AuthModule";
 import BodyModule from "@/store/modules/BodyModule";
-import ConfigModule from "@/store/modules/ConfigModule";
+import ConfigModule from "@/modules/pais-template/store/modules/ConfigModule";
 import ThemeModeModule from "@/store/modules/ThemeModeModule";
 
 config.rawError = true;
